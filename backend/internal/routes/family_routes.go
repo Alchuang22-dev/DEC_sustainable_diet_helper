1b5781d2988a9c27c1527bb8288d10c41c890f20
shenlayu <19910500882@163.com>
2024-11-27 17:13:34 +0800

    feat: family framework

--- backend/internal/routes/family_routes.go
+++ backend/internal/routes/family_routes.go
@@ -4,16 +4,30 @@ package routes
 import (
     "gorm.io/gorm"
     "github.com/Alchuang22-dev/DEC_sustainable_diet_helper/internal/controllers"
-    // "github.com/Alchuang22-dev/DEC_sustainable_diet_helper/internal/middleware"
+    "github.com/Alchuang22-dev/DEC_sustainable_diet_helper/internal/middleware"
 
     "github.com/gin-gonic/gin"
 )
 
 func RegisterFamilyRoutes(router *gin.Engine, db *gorm.DB) {
-    familyController := controllers.NewUserController(db)
+    familyController := controllers.NewFamilyController(db)
 
-    fanilyGroup := router.Group("/families")
+    familyGroup := router.Group("/families")
     {
-        // fanilyGroup.POST("/auth", familyController.)
+        // 需要认证的路由
+        authGroup := familyGroup.Group("")
+        authGroup.Use(middleware.AuthMiddleware())
+        {
+            // 创建家庭
+            authGroup.POST("/create", familyController.CreateFamily)
+            // 查看自己的家庭的信息
+            authGroup.POST("/details", familyController.FamilyDetails)
+            // 查看搜索家庭结果
+            authGroup.POST("/search", familyController.SearchFamily)
+            // 发送加入家庭请求
+            authGroup.POST("/join", familyController.JoinFamily)
+            // 批准加入家庭
+            authGroup.POST("/admit", familyController.AdmitJoinFamily)
+        }
     }
 }
